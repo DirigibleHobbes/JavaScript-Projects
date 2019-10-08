af33c861b7d198ee9004a35c619d1d7115501ca3
Medeskii <52930487+Medeskii@users.noreply.github.com>
2019-10-08 16:53:02 -0700

    Step 63
    Some JavaScript functions.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,8 @@
+function TellTime() {
+    text = document.getElementById ('Button').innerHTML = Date()
+}
+function ConcatenationAlliteration() {
+    var sentence = "Words intended to concatenate";
+    sentence += "with the use of the \"\+\=\" operator."
+    document.getElementById("Concatenate").innerHTML =  sentence;
+}
