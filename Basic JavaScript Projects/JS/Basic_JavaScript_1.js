68031a1d3e49d2da583d2654f82513153246862b
Medeskii <52930487+Medeskii@users.noreply.github.com>
2019-10-08 15:15:10 -0700

    JavaScript Step 58.
    Pushing my JavaScript Projects folder to GitHub.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,5 @@
+var A = "Yadda \"yadda\" yadda. ", B = "This is the second variable in this statement."
+var B = B.fontcolor("blue");
+document.write(B);
+var Name = "Mr. Bananas"
+document.write(Name);
