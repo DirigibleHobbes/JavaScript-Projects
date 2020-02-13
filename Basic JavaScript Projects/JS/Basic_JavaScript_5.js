a6824cf6f241f372a84e4031a89fbb53c73ce110
Medeskii <52930487+Medeskii@users.noreply.github.com>
2020-02-13 10:29:19 -0800

    Step 115 JavaScript
    Pushing Basic Java Script folder for step 115 of the JavaScript Course.

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,40 @@
+document.write(typeof "Word");
+document.write(typeof 3);
+function VariousNumbers() {
+    document.getElementById("Infinite").innerHTML = (4e9832)
+    document.getElementById("NegativeInfinity").innerHTML = (-5e2389)
+}
+
+document.write(13 > 4);
+document.write(13 < 4);
+console.log(7+5);
+document.write("14"+65);
+document.write(12-3==9);
+document.write(12-3==6);
+
+NumberOfToes1 = 10
+NumberOfToes2 = 10
+document.write(NumberOfToes1===NumberOfToes2);
+
+Politicians = 100
+Backbones = 1
+document.write(Politicians===Backbones);
+
+Pickles = 3
+FerrisWheels = 3
+document.write(Pickles===FerrisWheels);
+
+HamburgersEaten = 2
+HamburgersEaten1 = 3
+document.write(HamburgersEaten===HamburgersEaten1)
+
+document.write (4 > 2 && 6 > 5)
+document.write ( 4 > 7 && 4 > 3)
+
+document.write (4 > 3 || 5 > 7)
+document.write (3 > 5 || 5 > 9)
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(3 > 7)
+    document.getElementById("NotButIs").innerHTML = !(3<7)
+}
